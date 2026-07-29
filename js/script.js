cff106bcc4b04f31b516cf35ed98245d0cb5f6e4
Raga Deepthika Damaraju <damarajudeepthika@gmail.com>
2026-07-29 10:48:44 +0000

    eventlisteners

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
 alert("Welcome to NRIIT Learning Management System")
 let heading = document.getElementById
-("welcome");
-heading.innerHTML ="Welcome Future Software Engineers"
-console.log("Heading element:",heading)
+    ("welcome");
+heading.innerHTML = "Welcome Future Software Engineers"
+console.log("Heading element:", heading)
+let msg = document.getElementById("message")
+msg.innerHTML = "Javascript is fun"
+console.log("Message element:", msg)
+function showmessage() {
+    alert("Welcome to NRIIT Learning Management System")
+}
+function changeHeading() {
+    document.getElementById("welcome").
+        innerHTML = "Welcome Python Fullstack Developers"
+}
+let heading1 = document.querySelector("#Welcome");
+console.log("heading element:", heading1)
+let button = document.getElementById("btnGreeting");
+button.addEventListener("click", function () {
+    alert("welcome to javascript Event Handling");
+});
+let registerForm = document.getElementById("registerForm");
+registerForm.addEventListener("submit",function(event){
+    event.preventDefault();
+let name = document.getElementById
+("name").value;
+let email = document.getElementById 
+("email").value;
+let password = document.getElementById("password").value;
+if(!name ||  !email || !password) {
+        alert("Please fill in all fields.");
+        return;
+    }
+    alert("Registration successful!");
+    console.log("Name:",name);
+    console.log("Email:",email);
+    console.log("PasswordP:",password)});
+
